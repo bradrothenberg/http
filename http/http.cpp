837a0ce14af97587852fd72c95e83953a2959290
bradleyrothenberg <bradrothenberg@gmail.com>
2015-03-28 02:39:40 -0400

    gen guest token

--- http/http.cpp
+++ http/http.cpp
@@ -12,7 +12,6 @@
 
 
 
-
 using namespace web::http;
 using namespace web::http::client;
 using namespace web::json;
@@ -134,7 +133,7 @@ pplx::task<void> UploadFileToHttpServerAsync()
 }
 
 
-pplx::task<void> RequestJSONValueAsync()
+pplx::task<void> sparkGuestToken(json::value& val)
 {
 	// TODO: To successfully use this example, you must perform the request  
 	// against a server that provides JSON data.  
@@ -148,7 +147,7 @@ pplx::task<void> RequestJSONValueAsync()
 	request.set_body("grant_type=client_credentials");
 
 
-	return client.request(request).then([](http_response response) -> pplx::task<json::value>
+	return client.request(request).then([&val](http_response response) -> pplx::task<json::value>
 	{
 		if (response.status_code() == status_codes::OK)
 		{
@@ -158,12 +157,12 @@ pplx::task<void> RequestJSONValueAsync()
 		// Handle error cases, for now return empty json value... 
 		return pplx::task_from_result(json::value());
 	})
-		.then([](pplx::task<json::value> previousTask)
+		.then([&val](pplx::task<json::value> previousTask)
 	{
 		try
 		{
 			const json::value& v = previousTask.get();
-		
+			val = v;
 			std::string string;
 		//auto arrayJ = 	v.as_array();
 
@@ -178,6 +177,59 @@ pplx::task<void> RequestJSONValueAsync()
 			//wostringstream ss;
 			//ss << e.what() << endl;
 		//	wcout << ss.str();
+		}
+	});
+
+	/* Output:
+	Content-Type must be application/json to extract (is: text/html)
+	*/
+}
+
+
+pplx::task<void> sparkAccessToken(json::value& val,const utility::string_t& accessCode)
+{
+	// TODO: To successfully use this example, you must perform the request  
+	// against a server that provides JSON data.  
+	// This example fails because the returned Content-Type is text/html and not application/json.
+	http_client client(L"https://private-anon-575155ce3-sparkauthentication.apiary-proxy.com/api/v1/oauth/accesstoken");
+	// Manually build up an HTTP request with header and request URI.
+	http_request request(methods::POST);
+	request.headers().add(L"Content-Type", L"application/x-www-form-urlencoded");
+	request.headers().add(L"Authorization", L"Basic [WnByQWE3UDA1V01MWmFMNG53WFd0dU1FNlZldkIyN3Y6SUFaWFN6MWtIRnhWUzVWUA==]");
+
+	request.set_body(L"grant_type=authorization_code&"+accessCode + L"&response_type=code&redirect_uri=http://localhost/dev/SPARK/Sample-App-Auth-Drive-JS/callback.html");
+
+
+	return client.request(request).then([&val](http_response response) -> pplx::task<json::value>
+	{
+		if (response.status_code() == status_codes::OK)
+		{
+			return response.extract_json();
+		}
+
+		// Handle error cases, for now return empty json value... 
+		return pplx::task_from_result(json::value());
+	})
+		.then([&val](pplx::task<json::value> previousTask)
+	{
+		try
+		{
+			const json::value& v = previousTask.get();
+			val = v;
+			std::string string;
+			//auto arrayJ = 	v.as_array();
+
+
+			std::cout << "print" << std::endl;
+
+			// Perform actions here to process the JSON value...
+		}
+		catch (const http_exception& e)
+		{
+			// Print error.
+			//wostringstream ss;
+			//ss << e.what() << endl;
+			//	wcout << ss.str();
 		}
 	});
 
@@ -218,76 +270,62 @@ void IterateJSONValue(const json::value& objIn)
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	std::wcout << L"Calling RequestJSONValueAsync..." << std::endl;
-	RequestJSONValueAsync().wait();
 
-	std::wcout << L"Calling IterateJSONValue..." << std::endl;
-	//IterateJSONValue();
+	web::json::value get;
+	sparkAccessToken(get, L"hello").wait();
+
+	std::wcout << L"Calling RequestJSONValueAsync..." << std::endl;
+	sparkGuestToken(get).wait();
+
+	std::wcout << L"Guest Json Received:" << std::endl;
+
+	for (auto iter = get.as_object().cbegin(); iter != get.as_object().cend(); ++iter)
+	{
+		// Make sure to get the value as const reference otherwise you will end up copying 
+		// the whole JSON value recursively which can be expensive if it is a nested object. 
+		auto& str = iter->first;
+		const json::value &v = iter->second;
+
+		auto print = v.as_string();
+		// Perform actions here to process each string and value in the JSON object...
+		std::wcout << L"String: " << str << L", Value: " << v.as_string() << std::endl;
+	}
+
+
+	auto aCode = get[L"access_token"].as_string();
+	//std::string a =static_cast<std::string>( get[L"access_token"]);
+	std::wcout << aCode.c_str() << std::endl;
+
+	web::json::value get2;
+	sparkAccessToken(get2, aCode.c_str()).wait();
+
+
+
+
+	if(get2.size()>0){
+		std::wcout << L"Access json Received:" << std::endl;
+	for (auto iter = get2.as_object().cbegin(); iter != get2.as_object().cend(); ++iter)
+	{
+		// Make sure to get the value as const reference otherwise you will end up copying 
+		// the whole JSON value recursively which can be expensive if it is a nested object. 
+
+			auto& str = iter->first;
+			const json::value &v = iter->second;
+
+			auto print = v.as_string();
+			// Perform actions here to process each string and value in the JSON object...
+			std::wcout << L"String: " << str << L", Value: " << v.as_string() << std::endl;
+
+
+		}
+	}
+
+
+
+
 
 
 	auto fileStream = std::make_shared<ostream>();
-
-	// Open stream to output file.
-	pplx::task<void> requestTask = fstream::open_ostream(U("results.txt")).then([=](ostream outFile)
-	{
-		*fileStream = outFile;
-
-		// Create http_client to send the request.
-
-		http_client client(L"https://private-anon-575155ce3-sparkauthentication.apiary-proxy.com/api/v1/oauth/accesstoken");
-		// Manually build up an HTTP request with header and request URI.
-		http_request request(methods::POST);
-		request.headers().add(L"Content-Type", L"application/x-www-form-urlencoded");
-		request.headers().add(L"Authorization", L"Basic [WnByQWE3UDA1V01MWmFMNG53WFd0dU1FNlZldkIyN3Y6SUFaWFN6MWtIRnhWUzVWUA==]");
-
-		request.set_body("grant_type=client_credentials");
-
-
-		return client.request(request);
-	})
-
-		// Handle response headers arriving.
-		.then([=](http_response response)
-	{
-		printf("Received response status code:%u\n", response.status_code());
-
-		// Write response body into the file.
-		return response.body().read_to_end(fileStream->streambuf());
-	})
-
-		// Close the file stream.
-		.then([=](size_t)
-	{
-		return fileStream->close();
-	});
-
-	// Wait for all the outstanding I/O to complete and handle any exceptions
-	try
-	{
-		requestTask.wait();
-	}
-	catch (const std::exception &e)
-	{
-		printf("Error exception:%s\n", e.what());
-	}
-
-
-
-
-
-
-	std::wcout << L"Calling HTTPStreamingAsync..." << std::endl;
-	HTTPStreamingAsync().wait();
-
-	std::wcout << L"Calling HTTPRequestCustomHeadersAsync..." << std::endl;
-	HTTPRequestCustomHeadersAsync().wait();
-
-	std::wcout << L"Calling UploadFileToHttpServerAsync..." << std::endl;
-	UploadFileToHttpServerAsync().wait();
-
-
-
-
 
 	return 0;
 }
